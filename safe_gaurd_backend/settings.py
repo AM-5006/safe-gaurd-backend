90a359a4a2d109104e513a22cb52c4115958452d
AM-5006 <aniketmandrekar72576@gmail.com>
2024-02-06 20:44:07 +0530

    Updated settings

--- safe_gaurd_backend/settings.py
+++ safe_gaurd_backend/settings.py
@@ -82,12 +82,18 @@ WSGI_APPLICATION = 'safe_gaurd_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(env("DATABASE_URL")),
+    }
 
 
 # Password validation
